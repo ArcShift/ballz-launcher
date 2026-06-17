79c3eb8b4576a6038896930a0fb2b7964549dd4d
Kaafi <ma.kaafi@yahoo.co.id>
2026-06-17 18:17:14 +0700

    scale fit

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,9 @@ import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
 import { MainMenu } from './scenes/MainMenu';
 import { CampaignSelection } from './scenes/CampaignSelection';
-import { AUTO, Game } from 'phaser';
+import { AUTO, Game, Scale } from 'phaser';
 import { Preloader } from './scenes/Preloader';
+import * as Phaser from 'phaser';
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
@@ -24,6 +25,10 @@ const config: Phaser.Types.Core.GameConfig = {
     height: GH,
     parent: 'game-container',
     backgroundColor: '#028af8',
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     physics: {
         default: 'arcade',
         arcade: {
